25831f3f3b01182da8e60e25a851a7ed4eb0f0af
shekit <as7790@nyu.edu>
2016-12-05 03:33:39 -0500

    added video bg

--- public/js/global.js
+++ public/js/global.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
 
 
-	var video = $(".video-row")
+	var videoHeight = $("video").height()
+
+	$("video").bind("loadedmetadata", function(){
+		var height = this.videoHeight;
+		console.log(height)
+
+		//$(".top-intro").css({"min-height":height})
+	})
 
 	$("body").on('click', '.video-btn', scrollToPlace);
+
+	
 
 	function scrollToPlace(){
 		$('html, body').animate({
